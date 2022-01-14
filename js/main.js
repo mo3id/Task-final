2341f9bbc6dcc575e0e3f172559fafb6c8578b8e
Mohammed 3id <aboeid1650@gmail.com>
2022-01-14 02:28:08 +0200

    add audio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
 $(window).on("load", function () {
 
-    $("#loading").hide();
+   $("#loading").hide();
 
 });
 
@@ -11,49 +10,49 @@ window.onresize = changeSize;
 window.onload = changeSize;
 
 function changeSize(event) {
-        
-    $(".loader").fadeOut("slow");
-    
-    let newWidth = window.innerWidth;
-    
-    let newHeight = window.innerHeight;
-    
-    let myWidth = document.querySelector("main").clientWidth;
-    
-    let myHeight = document.querySelector("main").clientHeight
-    
-    
-    
-    if (newWidth <= myWidth) {
 
-        
-        let different = (myWidth - newWidth) / myWidth;
+   $(".loader").fadeOut("slow");
 
-        let scaleValue = `scale(${1 - different})`;
-        
-        document.querySelector("main").style.transition = "transform 500ms ease-in-out";
-        
-        document.querySelector("main").style.transform = scaleValue;
-        
-        document.querySelector("main").style.top = "0px";
-        
-    } else if (newHeight < myHeight) {
-        
-        let different2 = (myHeight - newHeight) / myHeight;
+   let newWidth = window.innerWidth;
 
-        let scaleValue2 = `scale(${1 - different2})`;
-        
-        document.querySelector("main").style.transition = "transform 500ms ease-in-out";
-        
-        document.querySelector("main").style.transform = scaleValue2;
-        
-        document.querySelector("main").style.top = "0px";
-        
-    }  else {
-        
-        document.querySelector("main").style.transform = 'scale(1)';
-    }
-    
+   let newHeight = window.innerHeight;
+
+   let myWidth = document.querySelector("main").clientWidth;
+
+   let myHeight = document.querySelector("main").clientHeight
+
+
+
+   if (newWidth <= myWidth) {
+
+
+      let different = (myWidth - newWidth) / myWidth;
+
+      let scaleValue = `scale(${1 - different})`;
+
+      document.querySelector("main").style.transition = "transform 500ms ease-in-out";
+
+      document.querySelector("main").style.transform = scaleValue;
+
+      document.querySelector("main").style.top = "0px";
+
+   } else if (newHeight < myHeight) {
+
+      let different2 = (myHeight - newHeight) / myHeight;
+
+      let scaleValue2 = `scale(${1 - different2})`;
+
+      document.querySelector("main").style.transition = "transform 500ms ease-in-out";
+
+      document.querySelector("main").style.transform = scaleValue2;
+
+      document.querySelector("main").style.top = "0px";
+
+   } else {
+
+      document.querySelector("main").style.transform = 'scale(1)';
+   }
+
 }
 
 
@@ -62,78 +61,373 @@ function changeSize(event) {
 
 var correct1 = document.getElementById('good-1');
 
-    correct2 = document.getElementById('good-2');
+correct2 = document.getElementById('good-2');
 
-    rong1 = document.getElementById('bad-1');
+rong1 = document.getElementById('bad-1');
 
-    rong2 = document.getElementById('bad-2');
+rong2 = document.getElementById('bad-2');
 
-    mainpage = document
+mainpage = document
 
-function good () {
+function good() {
 
-    if (correct1.classList.contains('correct')){
-        
-        $('#bad-1').addClass('disabled');
+   if (correct1.classList.contains('correct')) {
 
-        document.getElementById('right').style.display="block";
+      $('#bad-1').addClass('disabled');
+
+      document.getElementById('right').style.display = "block";
 
 
-    } else {
+   } else {
 
-            setTimeout(function() {
-            
-                            document.getElementById('wrong').style.display="block";
-            
-        }, 500);        
-    }
+      setTimeout(function () {
+
+         document.getElementById('wrong').style.display = "block";
+
+      }, 500);
+   }
+};
+$(window).on("load", function () {
+
+   $("#loading").hide();
+
+});
+
+
+window.onresize = changeSize;
+
+window.onload = changeSize;
+
+function changeSize(event) {
+
+   $(".loader").fadeOut("slow");
+
+   let newWidth = window.innerWidth;
+
+   let newHeight = window.innerHeight;
+
+   let myWidth = document.querySelector("main").clientWidth;
+
+   let myHeight = document.querySelector("main").clientHeight
+
+
+
+   if (newWidth <= myWidth) {
+
+
+      let different = (myWidth - newWidth) / myWidth;
+
+      let scaleValue = `scale(${1 - different})`;
+
+      document.querySelector("main").style.transition = "transform 500ms ease-in-out";
+
+      document.querySelector("main").style.transform = scaleValue;
+
+      document.querySelector("main").style.top = "0px";
+
+   } else if (newHeight < myHeight) {
+
+      let different2 = (myHeight - newHeight) / myHeight;
+
+      let scaleValue2 = `scale(${1 - different2})`;
+
+      document.querySelector("main").style.transition = "transform 500ms ease-in-out";
+
+      document.querySelector("main").style.transform = scaleValue2;
+
+      document.querySelector("main").style.top = "0px";
+
+   } else {
+
+      document.querySelector("main").style.transform = 'scale(1)';
+   }
+
+}
+
+
+var correct1 = document.getElementById('good-1');
+
+correct2 = document.getElementById('good-2');
+
+rong1 = document.getElementById('bad-1');
+
+rong2 = document.getElementById('bad-2');
+
+mainpage = document
+
+
+
+function good() {
+
+   if (!$('#bad-1').hasClass('disabled')) {
+
+      $('#bad-1').addClass('disabled');
+
+      document.getElementById('right').style.display = "block";
+
+      correctAudio();
+
+   } else {
+
+      //     setTimeout(function() {
+
+      //         document.getElementById('wrong').style.display="block";
+
+      // }, 500);        
+   }
 };
 
 
-function bad () {
+function good2() {
 
-    // if (rong1.classList.contains('in-correct')){
-        document.getElementById('wrong').style.display="block";
+   if (correct2.classList.contains('correct')) {
 
-        setTimeout(function() {
-            
-            document.getElementById('wrong').style.display="none";
+      $('#bad-2').addClass('disabled');
 
-}, 500);  
+      document.getElementById('right2').style.display = "block";
 
-    // } else {
+      correctAudio();
 
-        
-    // }
+   } else {
+
+      setTimeout(function () {
+
+         // document.getElementById('wrong').style.display="block";
+
+      }, 500);
+   }
 };
 
-function reload(){
+function bad() {
 
-    $('#bad-1').removeClass('disabled');
+   if (!$('#bad-1').hasClass('disabled')) {
 
-    document.getElementById('right').style.display="none";
-};
+      document.getElementById('wrong').style.display = "block";
 
-function showans(){
+      inCorrectAudio();
 
-    $('#bad-1').addClass('disabled');
+      setTimeout(function () {
 
-    document.getElementById('right').style.display="block";
-};
+         document.getElementById('wrong').style.display = "none";
 
+      }, 500);
 
-function next(){
-
-    document.getElementById('page-1').style.display="none";
-
-    document.getElementById('page-2').style.display="block";
+   } else {
 
 
-
+   }
 };
 
 
-function back(){
+function bad2() {
+
+   if ($('#bad-2').hasClass('disabled')) {
+
+
+   } else {
+
+      document.getElementById('wrong2').style.display = "block";
+
+      inCorrectAudio();
+
+      setTimeout(function () {
+
+         document.getElementById('wrong2').style.display = "none";
+
+
+      }, 500);
+   }
+};
+
+
+function reload() {
+
+   if ($('#page1').hasClass('active')) {
+
+      $('#bad-1').removeClass('disabled');
+
+      document.getElementById('right').style.display = "none";
+
+   } else {
+
+      $('#bad-2').removeClass('disabled');
+
+      document.getElementById('right2').style.display = "none";
+   };
+
+   // $('#bad-1').removeClass('disabled');
+
+   // document.getElementById('right').style.display="none";
+};
+
+function reloadall() {
+
+   back();
+
+   $('#bad-1').removeClass('disabled');
+
+   $('#bad-2').removeClass('disabled')
+
+   document.getElementById('right').style.display = "none";
+
+   document.getElementById('right2').style.display = "none";
+
+
+};
+
+function showans() {
+
+   if ($('#page1').hasClass('active')) {
+
+      $('#bad-1').addClass('disabled');
+
+      document.getElementById('right').style.display = "block";
+
+   } else {
+
+      $('#bad-2').addClass('disabled');
+
+      document.getElementById('right2').style.display = "block";
+   };
+
+
+};
+
+
+function next() {
+
+   document.getElementById("page1").style.display = "none";
+
+   document.getElementById("page2").style.display = "block";
+
+   $('#page2').addClass('active');
+
+   $('#page1').removeClass('active');
+
+   $('.next').addClass('disabled');
+
+   $('.back').removeClass('disabled');
+
+   document.getElementById("page-no").textContent = "2 of 2";
+
+};
+
+
+function back() {
+   $('#page1').addClass('active');
+
+   $('#page2').removeClass('active');
+
+   document.getElementById("page2").style.display = "none";
+
+   document.getElementById("page1").style.display = "block";
+
+   document.getElementById("page-no").textContent = "1 of 2";
+
+   $('.next').removeClass('disabled');
+
+   $('.back').addClass('disabled');
+
+}
+
+
+var av = document.getElementById("av-tag");
+var playTime = document.getElementsByClassName("play-time")[0];
+var playBtn=document.getElementsByClassName("play-btn")[0];
+var curTime=document.getElementById("cur-time");
+var volume = document.getElementById("volume");
+var speaker=document.getElementById("speaker");
+
+
+function correctAudio() {
+
+   document.getElementById("correct").play();
+}
+
+
+function inCorrectAudio() {
+
+   document.getElementById("incorrect").play();
+}
+
+
+function desctiptinAudio() {
+
+   document.getElementById("desctiptinAudio").play();
+
+   if (audio.paused == false) {
+      audio.pause();
+   } else {
+      audio.play();
+   }
+}
+
+
+function disAppeer1() {
+
+   document.getElementById("img-pause").style.display = "block"
+
+   document.getElementById("img-paly").style.display = "none"
+
+   document.getElementById("av-tag").play();
+}
+
+
+function disAppeer2() {
+
+   document.getElementById("img-pause").style.display = "none"
+
+   document.getElementById("img-paly").style.display = "block"
+
+   document.getElementById("av-tag").pause();
+}
+// ---------------------- Start Audio Player ----------------- //
+
+
+function bad() {
+
+   // if (rong1.classList.contains('in-correct')){
+   document.getElementById('wrong').style.display = "block";
+
+   setTimeout(function () {
+
+      document.getElementById('wrong').style.display = "none";
+
+   }, 500);
+
+   // } else {
+
+
+   // }
+};
+
+function reload() {
+
+   $('#bad-1').removeClass('disabled');
+
+   document.getElementById('right').style.display = "none";
+};
+
+function showans() {
+
+   $('#bad-1').addClass('disabled');
+
+   document.getElementById('right').style.display = "block";
+};
+
+
+function next() {
+
+   document.getElementById('page-1').style.display = "none";
+
+   document.getElementById('page-2').style.display = "block";
+
+
+
+};
+
+
+function back() {
 
 
 }
